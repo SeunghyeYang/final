21470997aa09a2da4032a9d09419d6210f6ce466
SeunghyeYang <71297657+SeunghyeYang@users.noreply.github.com>
2021-04-16 16:19:19 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $("button").click(function(){
+    $("p").click();
+  });
+});
+
+$.fn.tooltip.Constructor.VERSION // => "4.0.0"
+
+$('.carousel').carousel({
+  interval: 2000
+})
